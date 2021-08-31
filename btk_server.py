759035ece58483a134cfe69ff35e0096f56d249d
John Dong <32533877+johndongus@users.noreply.github.com>
2021-08-31 15:45:08 -0700

    Add files via upload

---
+++ btk_server.py
@@ -0,0 +1,147 @@
+#!/usr/bin/python3
+#
+# Bluetooth keyboard/Mouse emulator DBUS Service
+#
+
+from __future__ import absolute_import, print_function
+from optparse import OptionParser, make_option
+import os
+import sys
+import uuid
+import dbus
+import dbus.service
+import dbus.mainloop.glib
+import time
+import socket
+from gi.repository import GLib
+from dbus.mainloop.glib import DBusGMainLoop
+import logging
+from logging import debug, info, warning, error
+import bluetooth
+from bluetooth import *
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+class BTKbDevice():
+    MY_ADDRESS = "00:15:83:E9:07:C3"
+    MY_DEV_NAME = "SteelSeries_Rival_310_eSports_Mouse"
+
+    # define some constants
+    P_CTRL = 17  # Service port - must match port configured in SDP record
+    P_INTR = 19  # Interrupt port - must match port configured in SDP record
+    SDP_RECORD_PATH = sys.path[0] + "/sdp_record.xml"
+    UUID = "00001124-0000-1000-8000-00805f9b34fb"
+
+    def __init__(self):
+        print("2. Setting up BT device")
+        self.init_bt_device()
+        self.init_bluez_profile()
+
+    # configure the bluetooth hardware device
+    def init_bt_device(self):
+        print("3. Configuring Device name " + BTKbDevice.MY_DEV_NAME)
+        # set the device class to a keybord and set the name
+        os.system("hciconfig hci0 up")
+        os.system("hciconfig hci0 name " + BTKbDevice.MY_DEV_NAME)
+        # make the device discoverable
+        os.system("hciconfig hci0 piscan")
+
+    # set up a bluez profile to advertise device capabilities from a loaded service record
+    def init_bluez_profile(self):
+        print("4. Configuring Bluez Profile")
+        # setup profile options
+        service_record = self.read_sdp_service_record()
+        opts = {
+            "AutoConnect": True,
+            "ServiceRecord": service_record
+        }
+        # retrieve a proxy for the bluez profile interface
+        bus = dbus.SystemBus()
+        manager = dbus.Interface(bus.get_object(
+            "org.bluez", "/org/bluez"), "org.bluez.ProfileManager1")
+        manager.RegisterProfile("/org/bluez/hci0", BTKbDevice.UUID, opts)
+        print("6. Profile registered ")
+        os.system("hciconfig hci0 class 0x0025C0")
+
+    # read and return an sdp record from a file
+    def read_sdp_service_record(self):
+        print("5. Reading service record")
+        try:
+            fh = open(BTKbDevice.SDP_RECORD_PATH, "r")
+        except:
+            sys.exit("Could not open the sdp record. Exiting...")
+        return fh.read()
+
+    # listen for incoming client connections
+    def listen(self):
+        print("\033[0;33m7. Waiting for connections\033[0m")
+        self.scontrol = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_SEQPACKET, socket.BTPROTO_L2CAP)  # BluetoothSocket(L2CAP)
+        self.sinterrupt = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_SEQPACKET, socket.BTPROTO_L2CAP)  # BluetoothSocket(L2CAP)
+        self.scontrol.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sinterrupt.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        # bind these sockets to a port - port zero to select next available
+        self.scontrol.bind((socket.BDADDR_ANY, self.P_CTRL))
+        self.sinterrupt.bind((socket.BDADDR_ANY, self.P_INTR))
+
+        self.scontrol.listen(5)
+        self.sinterrupt.listen(5)
+
+        self.ccontrol, cinfo = self.scontrol.accept()
+        print (
+            "\033[0;32mGot a connection on the control channel from %s \033[0m" % cinfo[0])
+
+        self.cinterrupt, cinfo = self.sinterrupt.accept()
+        print (
+            "\033[0;32mGot a connection on the interrupt channel from %s \033[0m" % cinfo[0])
+
+    # send a string to the bluetooth host machine
+    def send_string(self, message):
+        try:
+            self.cinterrupt.send(bytes(message))
+        except OSError as err:
+            error(err)
+
+
+class BTKbService(dbus.service.Object):
+
+    def __init__(self):
+        print("1. Setting up service")
+        bus_name = dbus.service.BusName("org.thanhle.btkbservice", bus=dbus.SystemBus())
+        dbus.service.Object.__init__(self, bus_name, "/org/thanhle/btkbservice")
+        self.device = BTKbDevice()
+        self.device.listen()
+
+    @dbus.service.method('org.thanhle.btkbservice', in_signature='yay')
+    def send_keys(self, modifier_byte, keys):
+        print("Get send_keys request through dbus")
+        print("key msg: ", keys)
+        state = [ 0xA1, 1, 0, 0, 0, 0, 0, 0, 0, 0 ]
+        state[2] = int(modifier_byte)
+        count = 4
+        for key_code in keys:
+            if(count < 10):
+                state[count] = int(key_code)
+            count += 1
+        self.device.send_string(state)
+
+    @dbus.service.method('org.thanhle.btkbservice', in_signature='yay')
+    def send_mouse(self, modifier_byte, keys):
+        state = [0xA1, 2, 0, 0, 0, 0]
+        count = 2
+        for key_code in keys:
+            if(count < 6):
+                state[count] = int(key_code)
+            count += 1
+        self.device.send_string(state)
+
+if __name__ == "__main__":
+    try:
+        if not os.geteuid() == 0:
+            sys.exit("Only root can run this script")
+        DBusGMainLoop(set_as_default=True)
+        myservice = BTKbService()
+        loop = GLib.MainLoop()
+        loop.run()
+    except KeyboardInterrupt:
+        sys.exit()
---
+++ mouse_client.py
@@ -0,0 +1,178 @@
+#!/usr/bin/python3
+import threading
+import dbus
+import dbus.service
+import dbus.mainloop.glib
+import time
+import evdev
+from evdev import *
+import logging
+from logging import debug, info, warning, error
+import os
+import sys
+from select import select
+import pyudev
+import re
+import socket
+state = [0, 0, 0, 0];
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+def intercept():
+    global state,bus,btkservice,iface
+    bus = dbus.SystemBus()
+    btkservice = bus.get_object('org.thanhle.btkbservice', '/org/thanhle/btkbservice')
+    iface = dbus.Interface(btkservice, 'org.thanhle.btkbservice')
+
+    UDP_IP = "0.0.0.0"
+    UDP_PORT = 5005
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((UDP_IP, UDP_PORT))
+    while True:
+        data, addr = sock.recvfrom(1024)
+        X,Y = data.split(b',');
+        X = float(X)
+        Y = float(Y)
+        speed = 1
+        X = min(127, max(-127, int(X * speed))) & 255
+        Y = min(127, max(-127, int(Y * speed))) & 255
+        iface.send_mouse(0, [state[0],X,Y,state[3]])
+
+
+
+class InputDevice():
+    inputs = []
+
+    @staticmethod
+    def init():
+        context = pyudev.Context()
+        devs = context.list_devices(subsystem="input")
+        InputDevice.monitor = pyudev.Monitor.from_netlink(context)
+        InputDevice.monitor.filter_by(subsystem='input')
+        InputDevice.monitor.start()
+        for d in [*devs]:
+            InputDevice.add_device(d)
+
+    @staticmethod
+    def add_device(dev):
+        if dev.device_node == None or not re.match(".*/event\\d+", dev.device_node):
+            return
+        try:
+            if "ID_INPUT_MOUSE" in dev.properties:
+                print("detected mouse: " + dev.device_node)
+                InputDevice.inputs.append(MouseInput(dev.device_node))
+        except OSError:
+            error("Failed to connect to %s", dev.device_node)
+
+    @staticmethod
+    def remove_device(dev):
+        if dev.device_node == None or not re.match(".*/event\\d+", dev.device_node):
+            return
+        InputDevice.inputs = list(
+            filter(lambda i: i.device_node != dev.device_node, InputDevice.inputs))
+        print("Disconnected %s", dev)
+
+    @staticmethod
+    def set_leds_all(ledvalue):
+        for dev in InputDevice.inputs:
+            dev.set_leds(ledvalue)
+
+    @staticmethod
+    def grab(on):
+        if on:
+            for dev in InputDevice.inputs:
+                dev.device.grab()
+        else:
+            for dev in InputDevice.inputs:
+                dev.device.ungrab()
+
+    def __init__(self, device_node):
+        self.device_node = device_node
+        self.device = evdev.InputDevice(device_node)
+        self.device.grab()
+        info("Connected %s", self)
+
+    def fileno(self):
+        return self.device.fd
+
+    def __str__(self):
+        return "%s@%s (%s)" % (self.__class__.__name__, self.device_node, self.device.name)
+
+
+class MouseInput(InputDevice):
+    def __init__(self, device_node):
+        super().__init__(device_node)
+        self.state = state
+        self.x = 0
+        self.y = 0
+        self.z = 0
+        self.change = False
+        self.last = 0
+        self.bus = dbus.SystemBus()
+        self.btkservice = self.bus.get_object(
+            'org.thanhle.btkbservice', '/org/thanhle/btkbservice')
+        self.iface = dbus.Interface(self.btkservice, 'org.thanhle.btkbservice')
+        self.mouse_delay = 0.0001
+        self.mouse_speed = 1
+
+    def send_current(self, ir):
+        try:
+            self.iface.send_mouse(0, bytes(ir))
+        except OSError as err:
+            error(err)
+
+    def change_state(self, event):
+        if event.type == ecodes.EV_SYN:
+            current = time.monotonic()
+            diff = 0
+            if current - self.last < diff and not self.change:
+                return
+            self.last = current
+            speed = 1
+            self.state[1] = min(127, max(-127, int(self.x * speed))) & 255
+            self.state[2] = min(127, max(-127, int(self.y * speed))) & 255
+            self.state[3] = min(127, max(-127, self.z)) & 255
+            self.x = 0
+            self.y = 0
+            self.z = 0
+            self.change = False
+            self.send_current(self.state)
+        if event.type == ecodes.EV_KEY:
+            debug("Key event %s %d", ecodes.BTN[event.code], event.value)
+            self.change = True
+            if event.code >= 272 and event.code <= 276 and event.value < 2:
+                button_no = event.code - 272
+                if event.value == 1:
+                    self.state[0] |= 1 << button_no
+                else:
+                    self.state[0] &= ~(1 << button_no)
+        if event.type == ecodes.EV_REL:
+            if event.code == 0:
+                self.x += event.value
+            if event.code == 1:
+                self.y += event.value
+            if event.code == 8:
+                self.z += event.value
+
+    def get_info(self):
+        print("hello")
+
+    def set_leds(self, ledvalue):
+        pass
+
+
+if __name__ == "__main__":
+    InputDevice.init()
+    threading.Thread(target=intercept).start()
+    while True:
+        desctiptors = [*InputDevice.inputs, InputDevice.monitor]
+        r = select(desctiptors, [], [])
+       
+        for i in InputDevice.inputs:
+            try:
+                for event in i.device.read():
+                    i.change_state(event)
+            except OSError as err:
+                warning(err)
